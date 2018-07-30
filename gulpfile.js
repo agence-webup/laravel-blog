0b838508efec4b1db881656626a2b0d3f2872cf7
Robin PARISI <parisi.robin@gmail.com>
2018-07-30 17:14:11 +0200

    Add Higgcss

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,12 @@ const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 
  
-gulp.task('sass', function () {
+gulp.task( 'sass', function () {
     return gulp.src('./src/resources/assets/sass/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('../../public/vendor/laravel-blog/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/resources/assets/sass/**/*', gulp.series('sass'));
 });
