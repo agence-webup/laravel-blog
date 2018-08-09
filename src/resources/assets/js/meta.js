947bb7520d54530e80481cf551f842ffdfb4032d
Robin PARISI <parisi.robin@gmail.com>
2018-08-10 01:28:09 +0200

    POC for meta article data

---
+++ src/resources/assets/js/meta.js
@@ -0,0 +1,53 @@
+let Meta = (() => {
+
+    let model = [
+        'hyperlink',
+        'excerpt',
+        'isFeatured',
+        'isIndexed',
+        'seoTitle',
+        'seoDescription'
+    ];
+
+    class Meta {
+        constructor() {
+            this.fields = {};
+        }
+
+        init() {
+            this.queryElements();
+            this.bindEvents();
+        }
+
+        queryElements() {
+            model.forEach(elem => {
+                this.fields[elem] = document.getElementById(elem);
+            });
+        }
+
+        bindEvents() {
+            for (var prop in this.fields) {
+                this.fields[prop].addEventListener('change', (e) => {
+                    this.data();
+                });
+            }
+        }
+
+        data() {
+            let data = {};
+
+            for (var prop in this.fields) {
+                if(this.fields[prop].type === 'checkbox') {
+                    data[prop] = this.fields[prop].checked;
+                } else {
+                    data[prop] = this.fields[prop].value;
+                }
+            }; 
+
+            console.log(data);
+            return data;
+        }
+    }
+
+    return Meta;
+})();
